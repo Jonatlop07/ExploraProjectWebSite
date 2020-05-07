3bd5b9549b51a1a2d01c7f96cf51e19b6af012a0
Jonathan Lopez <jonatlop@gmail.com>
2020-05-07 18:38:29 -0500

    Se añaden los directorios correspondientes a los componentes de la aplicación y a las constantes. Además se incluye la biblioteca firebase

--- src/components/App/App.js
+++ src/components/App/App.js
@@ -1,25 +1,28 @@
-import React from "react";
+import React, { Component } from "react";
+import { BrowserRouter as Router, Switch, Route } from "react-router-dom";
+import NavBar from "../NavBar/NavBar.js";
+import Home from "../../pages/Home.js";
+import Login from "../../pages/Login.js";
+
 import logo from "../../logo.svg";
 import "./App.css";
 
 function App() {
+   const [] = React.useState(false);
    return (
-      <div className="App">
-         <header className="App-header">
-            <img src={logo} className="App-logo" alt="logo" />
-            <p>
-               Edit <code>src/App.js</code> and save to reload.
-            </p>
-            <a
-               className="App-link"
-               href="https://reactjs.org"
-               target="_blank"
-               rel="noopener noreferrer"
-            >
-               Learn React
-            </a>
-         </header>
-      </div>
+      <Router>
+         <NavBar />
+         <div>
+            <Switch>
+               <Route path="/login">
+                  <Login />
+               </Route>
+               <Route path="/">
+                  <Home />
+               </Route>
+            </Switch>
+         </div>
+      </Router>
    );
 }
 
--- src/index.js
+++ src/index.js
@@ -1,14 +1,28 @@
-import React from 'react';
-import ReactDOM from 'react-dom';
-import './index.css';
-import App from './App';
-import * as serviceWorker from './serviceWorker';
+import React from "react";
+import ReactDOM from "react-dom";
+import firebase from "firebase";
+import { BrowserRouter } from "react-router-dom";
+import App from "./components/App/App.js";
+import * as serviceWorker from "./serviceWorker";
+
+import "./index.css";
+
+firebase.initializeApp({
+   apiKey: "AIzaSyAgyWyR3fBJfyD2XxA5qCjKqDv7CebfVUc",
+   authDomain: "fir-react-auth-explora.firebaseapp.com",
+   databaseURL: "https://fir-react-auth-explora.firebaseio.com",
+   projectId: "fir-react-auth-explora",
+   storageBucket: "fir-react-auth-explora.appspot.com",
+   messagingSenderId: "1074941844633",
+   appId: "1:1074941844633:web:59bf929b0949d412406c08",
+   measurementId: "G-SK2XZ0S16D",
+});
 
 ReactDOM.render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>,
-  document.getElementById('root')
+   <React.StrictMode>
+      <App />
+   </React.StrictMode>,
+   document.getElementById("root")
 );
 
 // If you want your app to work offline and load faster, you can change
