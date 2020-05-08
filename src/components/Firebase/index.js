6c72f03453a5273f79142013bf50486d96807c02
Jonathan Lopez <jonatlop@gmail.com>
2020-05-07 20:03:21 -0500

    Se realiza la configuracion inicial de Firebase con React

---
+++ src/components/Firebase/index.js
@@ -0,0 +1,6 @@
+import FirebaseContext from "./context.js";
+import Firebase from "./firebase";
+
+export default Firebase;
+
+export { FirebaseContext };
--- src/index.js
+++ src/index.js
@@ -1,26 +1,17 @@
 import React from "react";
 import ReactDOM from "react-dom";
-import firebase from "firebase";
-import { BrowserRouter } from "react-router-dom";
+
 import App from "./components/App/App.js";
+import Firebase, { FirebaseContext } from "./components/Firebase";
+
 import * as serviceWorker from "./serviceWorker";
-
 import "./index.css";
-
-firebase.initializeApp({
-   apiKey: "AIzaSyAgyWyR3fBJfyD2XxA5qCjKqDv7CebfVUc",
-   authDomain: "fir-react-auth-explora.firebaseapp.com",
-   databaseURL: "https://fir-react-auth-explora.firebaseio.com",
-   projectId: "fir-react-auth-explora",
-   storageBucket: "fir-react-auth-explora.appspot.com",
-   messagingSenderId: "1074941844633",
-   appId: "1:1074941844633:web:59bf929b0949d412406c08",
-   measurementId: "G-SK2XZ0S16D",
-});
 
 ReactDOM.render(
    <React.StrictMode>
-      <App />
+      <FirebaseContext.Provider value={new Firebase()}>
+         <App />
+      </FirebaseContext.Provider>
    </React.StrictMode>,
    document.getElementById("root")
 );
