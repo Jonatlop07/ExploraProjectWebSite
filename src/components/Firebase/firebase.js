36ec7c4f23448160fd31c9f8fa9c63ed923a226c
Jonathan Lopez <jonatlop@gmail.com>
2020-05-08 22:08:52 -0500

    Se Plantea la consulta de los datos de los usuarios en la base de datos y se realiza el hosting con Firebase

--- src/components/Firebase/firebase.js
+++ src/components/Firebase/firebase.js
@@ -1,5 +1,6 @@
 import app from "firebase/app";
 import "firebase/auth";
+import "firebase/database";
 
 const config = {
    apiKey: process.env.REACT_APP_API_KEY,
@@ -17,6 +18,7 @@ class Firebase {
       app.initializeApp(config);
 
       this.auth = app.auth();
+      this.db = app.database();
    }
 
    doCreateUserWithEmailAndPassword = (email, password) =>
@@ -31,6 +33,10 @@ class Firebase {
 
    doPasswordUpdate = password =>
       this.auth.currentUser.updatePassword(password);
+
+   user = uid => this.db.ref(`users/${uid}`);
+
+   users = () => this.db.ref(`users`);
 }
 
 export default Firebase;
--- src/components/PasswordForget/PasswordForget.js
+++ src/components/PasswordForget/PasswordForget.js
@@ -6,7 +6,7 @@ import * as ROUTES from "../../constants/routes";
 
 const PasswordForgetPage = () => (
    <div>
-      <h1>PasswordForget</h1>
+      <h1>Password Forgot</h1>
       <PasswordForgetForm />
    </div>
 );
